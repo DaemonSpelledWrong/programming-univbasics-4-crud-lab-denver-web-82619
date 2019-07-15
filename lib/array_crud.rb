a584e77d51664a9bae078ab6de6ffee9a2e910c1
Damon Self <damongself@gmail.com>
2019-07-15 09:56:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  myArray = Array.new(0..2)
+  myArray = ["cat", "dog", "bear", "turtle"]
 end
 
 def add_element_to_end_of_array(array, element)
