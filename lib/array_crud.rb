ce8002e6a95741f9bb66365cb33cd3ef051d4fcc
Damon Self <damongself@gmail.com>
2019-07-15 10:01:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,7 +31,7 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  array.slice(-1) 
+  array.slice(-1) # => Using slice instead of last for variety in this lab.
 end
 
 def update_element_from_index(array, index_number, element)
